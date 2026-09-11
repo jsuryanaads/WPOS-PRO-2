db7a34e3ffbf51134e8fda146fa3f967bf0c1494
Jajang Suryana <jsuryanaads@gmail.com>
2026-09-12 02:57:43 +0700

    chore(release): bump version to 2.3.5

--- app/config.py
+++ app/config.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 APP_NAME = "WPOS PRO 2"
-APP_VERSION = "2.3.4"
+APP_VERSION = "2.3.5"
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 if getattr(sys, "frozen", False) and os.name == "nt":
