f0656883a2f176125772d3e3ed3e85c8d2212302
Jajang Suryana <jsuryanaads@gmail.com>
2026-09-12 04:28:25 +0700

    feat(excel): release WPOS PRO 2 v2.4.0

--- app/config.py
+++ app/config.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 APP_NAME = "WPOS PRO 2"
-APP_VERSION = "2.3.16"
+APP_VERSION = "2.4.0"
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 if getattr(sys, "frozen", False) and os.name == "nt":
