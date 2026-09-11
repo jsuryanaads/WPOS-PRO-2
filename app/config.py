045ac629fcce4e367a6cb3365f1ba83a797141ed
Jajang Suryana <jsuryanaads@gmail.com>
2026-09-12 01:34:01 +0700

    bump: set WPOS PRO version to 2.0.2

--- app/config.py
+++ app/config.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 APP_NAME = "WPOS PRO 2"
-APP_VERSION = "2.0.1"
+APP_VERSION = "2.0.2"
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 if getattr(sys, "frozen", False) and os.name == "nt":
