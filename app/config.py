6f5ac72793f7b96210133240bcae72e5565d673e
Jajang Suryana <jsuryanaads@gmail.com>
2026-09-12 18:24:50 +0700

    release: set WPOS PRO 2 version 2.8.2

--- app/config.py
+++ app/config.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 APP_NAME = "WPOS PRO 2"
-APP_VERSION = "2.8.1"
+APP_VERSION = "2.8.2"
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 if getattr(sys, "frozen", False) and os.name == "nt":
