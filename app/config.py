2b3e0fb243cfaf076b1bb261e7d43a917233a4f4
Jajang Suryana <jsuryanaads@gmail.com>
2026-09-16 19:52:59 +0700

    Bump minor release to 2.11.0

--- app/config.py
+++ app/config.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 APP_NAME = "WPOS PRO 2"
-APP_VERSION = "2.10.1"
+APP_VERSION = "2.11.0"
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 if getattr(sys, "frozen", False) and os.name == "nt":
