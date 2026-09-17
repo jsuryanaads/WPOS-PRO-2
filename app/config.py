4882d2101bb0c274e1e534e900d784a738ef7a12
We DESIGN <jsuryanaads@gmail.com>
2026-09-17 09:50:02 +0700

    Bump version to 2.13.1 for premium login release

--- app/config.py
+++ app/config.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 APP_NAME = "WPOS PRO 2"
-APP_VERSION = "2.13.0"
+APP_VERSION = "2.13.1"
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 if getattr(sys, "frozen", False) and os.name == "nt":
